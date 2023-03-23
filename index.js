fe4202bfc6a44f6cdc36bddf58d99f39cc2c8dc6
Fatima-Shokuhi <102737996+Fatima-Shokuhi@users.noreply.github.com>
2023-03-23 03:41:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
-const upDate = new Date('8 apr 2022')
+const upDate = new Date('8 apr 2023')
 
 function countDown(){
     
